663b9d6abcf62a42b51cf7c9a38cf6a5ec3882d1
Wes <wes@goulet.dev>
2024-12-06 12:25:39 -0800

    simplify sw

--- public/sw.js
+++ public/sw.js
@@ -6,15 +6,15 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
-      // HERE IS THE BUG - just checking of body is undefined causes the body
+      // HERE IS THE BUG - just checking of body is truthy causes the body
       // to not be included in the fetch, server does not receive the body
-      if (event.request.body !== undefined) {
+      if (event.request.body) {
         console.log("this request has a body");
       }
       const response = await fetch(event.request);
 
-      // to aid in debugging/logging we will throw an extra header on the response
-      const responseWithHeader = new Response(null, {
+      // to aid in debugging/logging we will add an extra header on the response
+      const responseWithHeader = new Response(response.body, {
         status: response.status,
         headers: { ["X-Service-Worker"]: "true" },
       });
